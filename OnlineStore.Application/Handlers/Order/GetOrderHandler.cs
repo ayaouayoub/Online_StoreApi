3ed0c573f3df9501ecee8c3079d10ad51bdbca8a
Ayoub Ayaou <ayaouayoub70@gmail.com>
2026-08-11 18:02:05 +0200

    Add new access rule, who owns this order?

--- OnlineStore.Application/Handlers/Order/GetOrderHandler.cs
+++ OnlineStore.Application/Handlers/Order/GetOrderHandler.cs
@@ -7,13 +7,17 @@ using OnlineStore.Application.Dtos;
 using OnlineStore.Application.Exceptions;
 using OnlineStore.Application.Handlers.Order.Mappings;
 using OnlineStore.Application.Handlers.Order.Queries;
+using OnlineStore.Application.Interfaces;
 using OnlineStore.Application.Interfaces.Repositories;
+using OnlineStore.Domain.Enums;
 
 namespace OnlineStore.Application.Handlers.Order
 {
     public sealed class GetOrderHandler
     {
         private readonly IOrderRepository _orderRepository;
+        private readonly ICurrentUser _currentUser;
+        private readonly ICustomerRepository _customerRepository;
 
         public GetOrderHandler(IOrderRepository orderRepository)
         {
@@ -23,6 +27,12 @@ namespace OnlineStore.Application.Handlers.Order
         public async Task<OrderDto> ExecuteAsync(GetOrderQuery query)
         {
             var order = await _orderRepository.GetByIdAsync(query.OrderId) ?? throw new NotFoundException($"Order {query.OrderId} not found.");
+            var user = _currentUser.User;
+            if (user.RoleId == (int)RoleType.Customer)
+            {
+                var details = await _customerRepository.GetByUserIdAsync(user.Id) ?? throw new ForbiddenException("Customer not found.");
+                if (order.CustomerId != details.Customer.Id) throw new ForbiddenException("You cannot access this order.");
+            }
             return order.ToDto();
         }
     }
